0bb16e8cfe40a291da38ec995806f5523046faaf
salamander5777 <77403008+salamander5777@users.noreply.github.com>
2021-10-24 02:40:22 -0400

    Update App.java

--- src/main/java/ucf/assignments/App.java
+++ src/main/java/ucf/assignments/App.java
@@ -1,5 +1,10 @@
 package ucf.assignments;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Michael Gilday
+ */
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
